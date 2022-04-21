c7a3fb6e20c5cf77d59f32e72907342c6287aee4
shahzodkuchkarov <shahzodk0@gmail.com>
2022-04-21 18:36:44 +0200

    finished company list page

--- src/Pages/Dashboard/CompanyList.tsx
+++ src/Pages/Dashboard/CompanyList.tsx
@@ -1,24 +1,60 @@
-import React from "react";
-import { Input } from "antd";
+import React, { useState } from "react";
+import { Input, Table } from "antd";
 import Title from "components/title";
 import Axios from "utils/axios";
 
-const { Search } = Input;
+interface Company {
+  "1. symbol": string;
+  "2. name": string;
+  "3. type": string;
+  "4. region": string;
+  "5. marketOpen": string;
+  "6. marketClose": string;
+  "7. timezone": string;
+  "8. currency": string;
+  "9. matchScore": string;
+}
 
 const CompanyList = () => {
+  const [companies, setCompanies] = useState<Company[]>([]);
+  const [selectedCompanies, setSelectedCompanies] = useState<Company[]>([]);
+  const columns = [
+    {
+      title: "Company Name",
+      dataIndex: "2. name",
+      key: "name",
+    },
+    {
+      title: "Symbol",
+      dataIndex: "1. symbol",
+      key: "symbol",
+    },
+    {
+      title: "Action",
+      dataIndex: "",
+      key: "action",
+      render: (text: string, record: Company) => (
+        <button onClick={() => console.log(record)}>remove</button>
+      ),
+    },
+  ];
   const onSearch = (value: string) => {
     Axios.get(
       `?function=SYMBOL_SEARCH&keywords=${value}&apikey=${process.env.REACT_APP_API_KEY}`
     ).then((res) => {
-      console.log(res.data.bestMatches);
+      const { bestMatches } = res.data;
+      setCompanies(bestMatches);
     });
+  };
+  const onSelect = (company: Company) => {
+    setSelectedCompanies((prev) => Array.from(new Set([...prev, company])));
   };
   return (
     <div>
       <div className="companyName">
         <div className="search">
           <Title text="Company Name" />
-          <Search
+          <Input.Search
             placeholder="input search text"
             onSearch={onSearch}
             style={{ width: 200 }}
@@ -26,8 +62,16 @@ const CompanyList = () => {
         </div>
         <div className="results">
           <Title text="Search Results" />
-          <div className="results__list"></div>
+          <div className="results__list">
+            {companies.map((company) => (
+              <div key={company["1. symbol"]}>
+                {company["1. symbol"]} - {company["2. name"]}
+                <button onClick={() => onSelect(company)}>+</button>
+              </div>
+            ))}
+          </div>
         </div>
+        <Table dataSource={selectedCompanies} columns={columns} />
       </div>
     </div>
   );
--- src/redux/index.ts
+++
@@ -1,8 +0,0 @@
-import { applyMiddleware } from "redux";
-import { createStore } from "redux";
-import rootReducer from "./reducers";
-
-export const store = createStore(rootReducer, applyMiddleware());
-
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
